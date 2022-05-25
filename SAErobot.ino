5ea466077d40367c246f500693d1854810f6b386
Liwis779 <95344837+Liwis779@users.noreply.github.com>
2022-05-25 14:48:36 +0200

    Add files via upload

--- SAErobot.ino
+++ SAErobot.ino
@@ -5,166 +5,189 @@ float  nbTourD = 0;
 float  compteurG = 0;
 float  compteurD = 0;
 char   message;
-String commande= "";
-
-
+String commande = "";
+int    vitesseRoue = 255;
+bool   cond = true;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(9600);
- // while(!Serial1){;}
+  // while(!Serial1){;}
   Serial.begin(9600);
- // while(!Serial){;}
-  pinMode(12,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(2,INPUT_PULLUP);
-  pinMode(4,INPUT_PULLUP);
-  pinMode(13,INPUT_PULLUP);
-  pinMode(3,INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2),interruptG,RISING);
-  attachInterrupt(digitalPinToInterrupt(3),interruptD,FALLING);
+  // while(!Serial){;}
+  pinMode(12, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), interruptG, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), interruptD, FALLING);
+  attachInterrupt(digitalPinToInterrupt(0), interruptBT, RISING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly
-  if(Serial1.available()){
-      message = (char)Serial1.read();
-      commande+=message;
-      Serial.println(message);
-      commande="";
-  }
-    action();
-    message=' ';
+  message = ' ';
 }
 
-void Script(bool(*inter)(void),int distanceR,int distanceL,int Rspeed,int Lspeed)
+void Script(bool(*inter)(void), int distanceR, int distanceL, int Rspeed, int Lspeed)
 {
-    float Rdist = distanceR;
-    float Ldist = distanceL;
-    compteurD = 0;
-    compteurG = 0;
-    bool cond = true;
-    while(cond)
+  float Rdist = distanceR;
+  float Ldist = distanceL;
+  compteurD = 0;
+  compteurG = 0;
+  cond = true;
+  while (cond)
+  {
+    float dist = getDist();
+    Serial.println(dist);
+    sendBT(dist);
+    if (dist < 30)
     {
-      float dist = getDist();
-      Serial.println(dist);
-      sendBT(dist);
-      if(dist < 30)
+      float tempG = compteurG;
+      float tempD = compteurD;
+      cond = inter();
+      if (cond == true)
       {
-		    float tempG = compteurG;
-	    	float tempD = compteurD;
-        cond = inter();
-		    if(cond == true)
-		    {
-			    compteurG = tempG;
-			    compteurD = tempD;
-		    }
-      }
-      if(cond){
-        RWheel(Rspeed);
-        LWheel(Lspeed);
-      }
-      if(Rdist < abs(compteurD) && Ldist < abs(compteurG))
-      {
-        cond = false;  
+        compteurG = tempG;
+        compteurD = tempD;
       }
     }
-    
+    if (cond) {
+      RWheel(Rspeed);
+      LWheel(Lspeed);
+    }
+    if (Rdist < abs(compteurD) && Ldist < abs(compteurG))
+    {
+      cond = false;
+    }
+  }
+
 }
+
 bool StopSTP()
 {
   Serial.println("wallah ta vu le mur");
-  for(int i = 0; i < 5; i++){
-  Serial.print("mec ya un mur je teste numero: ");
-  Serial.println(i);
+  for (int i = 0; i < 5; i++) {
+    Serial.print("mec ya un mur je teste numero: ");
+    Serial.println(i);
     float distance = getDist();
-    while(distance < 35)
+    while (distance < 35)
     {
       distance = getDist();
       Serial.println(distance);
       RWheel(-255);
-      LWheel(-255);  
+      LWheel(-255);
     }
-  
+
   }
-  return false;  
+  return false;
 }
+
 bool StoppasSTP()
 {
-  return true;  
+  return true;
 }
-void action(){
+
+void action() {
   compteurG = 0;
   compteurD = 0;
   //Serial.println(commande);
-    if(message == 'A'){
-      Script(StopSTP,200,200,255,255);
-    }else if(message == 'R'){
-      Script(StoppasSTP,200,200,-255,-255);
-    }else if(message == 'G'){
-      Script(StoppasSTP,100,100,255,-255);
-    }else if(message == 'D'){
-      Script(StoppasSTP,100,100,-255,255);
-  }else{
-        LWheel(0);
-        RWheel(0);
+  if (message == 'A') {
+    Script(StopSTP, 200, 200, vitesseRoue, vitesseRoue);
+  } else if (message == 'R') {
+    Script(StoppasSTP, 200, 200, -vitesseRoue, -vitesseRoue);
+  } else if (message == 'G') {
+    Script(StoppasSTP, 100, 100, vitesseRoue, -vitesseRoue);
+  } else if (message == 'D') {
+    Script(StoppasSTP, 100, 100, -vitesseRoue, vitesseRoue);
+  } else if (message == 'E') {
+    Script(StoppasSTP, 50, 50, vitesseRoue, -vitesseRoue);
+  } else if (message == 'O') {
+    Script(StoppasSTP, 50, 50, -vitesseRoue, vitesseRoue);
+  } else if (message == 'e') {
+    Script(StoppasSTP, 50, 50, vitesseRoue, -vitesseRoue);
+  } else if (message == 'o') {
+    Script(StoppasSTP, 50, 50, -vitesseRoue, vitesseRoue);
+  } else if (message == 'V') {
+    int i = 0;
+    char VitesseM[3];
+    while (message == 'T') {
+      VitesseM[i] = (char)Serial1.read();
+      i++;
     }
-}
-void interruptG(){
-  if (digitalRead(2)==digitalRead(4)){
-      compteurG++;
-  }
-  else{
-      compteurG--;
+    vitesseRoue = atoi(VitesseM);
+    Serial.print(VitesseM);
+  } else {
+    LWheel(0);
+    RWheel(0);
   }
 }
 
-void interruptD(){
-    if (digitalRead(3)==digitalRead(13)){
-      compteurD++;
+void interruptBT() {
+  if (Serial1.available()) {
+    message = (char)Serial1.read();
+    Serial.println(message);
   }
-  else{
-      compteurD--;
+  cond = false;
+}
+
+void interruptG() {
+  if (digitalRead(2) == digitalRead(4)) {
+    compteurG++;
+  }
+  else {
+    compteurG--;
   }
 }
 
-void sendBT(float number){
-      char text[5];
-      dtostrf(number,0,4,text);
-      Serial1.write(text);
-      Serial1.write("\n");
+void interruptD() {
+  if (digitalRead(3) == digitalRead(13)) {
+    compteurD++;
+  }
+  else {
+    compteurD--;
+  }
+}
+
+void sendBT(float number) {
+  char text[5];
+  dtostrf(number, 0, 4, text);
+  Serial1.write(text);
+  Serial1.write("\n");
 }
 
 float getDist()
 {
-  pinMode(SOUND,OUTPUT);
-  digitalWrite(SOUND,HIGH);
+  pinMode(SOUND, OUTPUT);
+  digitalWrite(SOUND, HIGH);
   delayMicroseconds(8);
-  digitalWrite(SOUND,LOW);
-  pinMode(SOUND,INPUT);
-  float dist = pulseIn(SOUND,HIGH)/58.0;
+  digitalWrite(SOUND, LOW);
+  pinMode(SOUND, INPUT);
+  float dist = pulseIn(SOUND, HIGH) / 58.0;
   return dist;
 }
 
 void RWheel(int vitesse)
 {
-    if(vitesse >= 0){
-      digitalWrite(12,HIGH);
-    }else{
-      digitalWrite(12,LOW);
-      }
-    analogWrite(10,abs(vitesse));
+  if (vitesse >= 0) {
+    digitalWrite(12, HIGH);
+  } else {
+    digitalWrite(12, LOW);
+  }
+  analogWrite(10, abs(vitesse));
 }
 
 void LWheel(int vitesse)
 {
-    if(vitesse >= 0){
-      digitalWrite(8,LOW);
-    }else{
-      digitalWrite(8,HIGH);
-      }
-    analogWrite(9,abs(vitesse));
+  if (vitesse >= 0) {
+    digitalWrite(8, LOW);
+  } else {
+    digitalWrite(8, HIGH);
+  }
+  analogWrite(9, abs(vitesse));
 }
